f20704dfc22b38edd13b6dd1a9a9fc6e62c8e2d5
Sevdanur GENC <sevdanurgenc@gmail.com>
2020-07-18 18:59:49 +0300

    Add files via upload

---
+++ MvcTicSys/MvcTicSys/Models/KullaniciGirisi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcTicSys.Models
+{
+    public class KullaniciGirisi
+    {
+        [Required]
+        [Display(Name = "Kullanici Adi : ")]
+        public string KullaniciAdi { get; set; }
+
+        [Required]
+        [Display(Name = "Sifre : ")]
+        public string Sifre { get; set; }
+
+
+    }
+}
